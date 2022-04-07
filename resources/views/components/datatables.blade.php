ffe5eb147d7732195d7a159f12a29c08e6f8185c
hasinilmalik <hasinilmalik@gmail.com>
2022-04-07 12:53:57 +0700

    delete datepicker js

--- resources/views/components/datatables.blade.php
+++ resources/views/components/datatables.blade.php
@@ -7,26 +7,6 @@
                 $('#datatable').DataTable({
                     "lengthChange": false,
                     "pageLength": 100,
-                    // "lengthMenu": [
-                    //     [10, 25, 50, -1],
-                    //     [10, 25, 50, "All"]
-                    // ]
-                });
-            });
-        </script>
-
-        <script>
-            $(function() {
-                $('.date-picker').datepicker({
-                    changeMonth: true,
-                    changeYear: true,
-                    showButtonPanel: true,
-                    dateFormat: 'MM yy',
-                    onClose: function(dateText, inst) {
-                        var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
-                        var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
-                        $(this).datepicker('setDate', new Date(year, month, 1));
-                    }
                 });
             });
         </script>
