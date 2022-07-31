749c0641aa670ed00206a250d289e266a170c6dc
hasinilmalik <hasinilmalik@gmail.com>
2022-07-31 18:37:20 +0700

    php artisan make:command BackupDatabase

--- app/Models/Presence.php
+++ app/Models/Presence.php
@@ -7,23 +7,28 @@ use Illuminate\Database\Eloquent\Model;
 
 class Presence extends Model
 {
-    use HasFactory;
-    // public $timestamps = false;
-    protected $table = 'presences';
-      protected $fillable = [
-         'teacher_id',
-         'date',
-         'time_in',
-         'time_out',
-         'is_late',
-         'overtime',
-         'note',
-         'created_at',
-         'updated_at'
-      ];
-    protected $with = ['teacher'];
+   use HasFactory;
+   // public $timestamps = false;
+   protected $table = 'presences';
+   protected $fillable = [
+      'teacher_id',
+      'date',
+      'time_in',
+      'time_out',
+      'is_late',
+      'overtime',
+      'note',
+      'created_at',
+      'updated_at'
+   ];
+   protected $with = ['teacher'];
 
-    public function teacher()
+   /**
+    * teacher
+    *
+    * @return void
+    */
+   public function teacher()
    {
       return $this->belongsTo(Teacher::class);
    }
--- app/View/Components/Modal.php
+++ app/View/Components/Modal.php
@@ -9,9 +9,14 @@ class Modal extends Component
     public $id;
     public function __construct($id)
     {
-        $this->id=$id;
+        $this->id = $id;
     }
-    
+
+    /**
+     * render
+     *
+     * @return void
+     */
     public function render()
     {
         return view('components.modal');
